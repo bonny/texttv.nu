3b067bd0ac901e81b0fa8d9c25b0a72e71a6bf4d
Pär Thernström <par@earthpeople.se>
2014-01-05 19:12:35 +0100

    misc fixes

--- app/js/app.js
+++ app/js/app.js
@@ -27,17 +27,38 @@ angular.module('texttv', ['ionic'])
 
 	*/
 
+	$scope.startPages = {
+		firstRow: [
+			{ title: "Nyheter", range: "100" },
+			{ title: "Ekonomi", range: "200" },
+			{ title: "Sport", range: "300" },
+			{ title: "Väder", range: "400" }
+		],
+		secondRow: [
+			{ title: "Blandat", range: "500" },
+			{ title: "På TV", range: "600" },
+			{ title: "Innehåll", range: "700" },
+			{ title: "UR", range: "800" }
+		]
+	};
+
 	$scope.sidebar = {
 		pages: [
 			{ title: "Start", range: "100" },
-			{ title: "Nyheter", range: "100-104" },
+			{ title: "Nyheter", range: "100-105" },
 			{ title: "Inrikes", range: "101-102" },
 			{ title: "Utrikes", range: "104-105" },
+			{ title: "Ekonomi", range: "200" },
 			{ title: "Sport", range: "300-301" },
 			{ title: "Sportresultat", range: "377" },
-			{ title: "Sportresultat, V75", range: "571" }
+			{ title: "Sportresultat, V75", range: "571" },
+			{ title: "Väder", range: "400" },
+			{ title: "Blandat", range: "500" },
+			{ title: "På TV", range: "600" },
+			{ title: "Innehåll", range: "700" },
+			{ title: "UR", range: "800" }
 		]
-	}
+	};
 
 	/**
 	 * Show right sidebar
