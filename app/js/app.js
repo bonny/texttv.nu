0767182f57087c178f1b91150e26b93228e4fa5b
Pär Thernström <par@earthpeople.se>
2014-01-05 17:49:52 +0100

    add hand picked favs to sidebar

---
+++ app/js/app.js
@@ -0,0 +1,100 @@
+
+angular.module('texttv', ['ionic'])
+.controller('TexttvCtrl', function($scope) {
+
+  $scope.tasks = [
+		{ title: 'Collect coins' },
+		{ title: 'Eat mushrooms' },
+		{ title: 'Get high enough to grab the flag' },
+		{ title: 'Find the Princess' }
+	];
+
+	/*
+      Sport, Resultat377
+      Sport, Resultat V75571
+      Leifbys text-tv-favoriter
+      1) Målservice 377
+      2) Skytteligor (fotboll) 351
+      3) Tipset i sista stund 327
+      4) Resultat/Tabellbörsen 330
+      5) Tipset 551
+      Dela på Twitter
+      Dela på Facebook
+      Permalänk
+      Blogg Om TextTV.nu
+      Ladda hem vår app
+      för Iphone/Ipad
+
+	*/
+
+	$scope.sidebar = {
+		pages: [
+			{ title: "Start", range: "100" },
+			{ title: "Nyheter", range: "100-104" },
+			{ title: "Inrikes", range: "101-102" },
+			{ title: "Utrikes", range: "104-105" },
+			{ title: "Sport", range: "300-301" },
+			{ title: "Sportresultat", range: "377" },
+			{ title: "Sportresultat, V75", range: "571" }
+		]
+	}
+
+	/**
+	 * Show right sidebar
+	 */
+	$scope.toggleMenu = function() {
+		$scope.sideMenuController.toggleRight();
+	};
+
+});
+
+var mySwiper;
+$(function($) {
+
+  mySwiper = new Swiper('.swiper-container',{
+	//Your options here:
+	mode: 'horizontal',
+	resistance: true,
+	useCSS3Transforms: true,
+	/*onSlideChangeStart: function(swiper) {
+	  console.log("onSlideChangeStart");
+	},
+	onSlideChangeEnd: function(swiper) {
+	  console.log("onSlideChangeEnd");
+	},
+	onMomentumBounce: function() {
+		console.log("onMomentumBounce");
+	},
+	onResistanceBefore: function() {
+		console.log("onResistanceBefore");
+	},
+	onResistanceAfter: function() {
+		console.log("onResistanceAfter");
+	},
+	onSetWrapperTransition: function(swiper, duration) {
+		console.log("onSetWrapperTransition", duration);	
+	},
+	onSetWrapperTransform: function (swiper, transform) {
+		console.log("onSetWrapperTransform", transform);
+	}*/
+  });
+
+  $.getJSON("http://texttv.nu/api/get/100", function(page) {
+
+	// console.log(page);
+	// console.log(mySwiper);
+	
+	var newSlide = mySwiper.createSlide( "Prev" );
+	newSlide.append();
+
+	newSlide = mySwiper.createSlide(page[0].content[0]);
+	newSlide.append();
+
+	newSlide = mySwiper.createSlide( "Next" );
+	newSlide.append();
+
+	mySwiper.swipeTo(1);
+
+  });
+
+}, false);
