055655760b17a288a52cf451d5cf45562e49d1c6
Pär Thernström <par.thernstrom@gmail.com>
2018-04-29 20:44:14 +0200

    Update Cordova Ios to 4.5.4

--- app/texttvnucodovaapp/platforms/ios/platform_www/cordova_plugins.js
+++ app/texttvnucodovaapp/platforms/ios/platform_www/cordova_plugins.js
@@ -1,92 +1,92 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
-    {
-        "id": "cordova-plugin-admobpro.AdMob",
-        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
-        "pluginId": "cordova-plugin-admobpro",
-        "clobbers": [
-            "window.AdMob"
-        ]
-    },
-    {
-        "id": "cordova-plugin-google-analytics.UniversalAnalytics",
-        "file": "plugins/cordova-plugin-google-analytics/www/analytics.js",
-        "pluginId": "cordova-plugin-google-analytics",
-        "clobbers": [
-            "analytics",
-            "ga"
-        ]
-    },
-    {
-        "id": "cordova-plugin-inappbrowser.inappbrowser",
-        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-        "pluginId": "cordova-plugin-inappbrowser",
-        "clobbers": [
-            "cordova.InAppBrowser.open",
-            "window.open"
-        ]
-    },
-    {
-        "id": "cordova-plugin-console.console",
-        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
-        "pluginId": "cordova-plugin-console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "id": "cordova-plugin-console.logger",
-        "file": "plugins/cordova-plugin-console/www/logger.js",
-        "pluginId": "cordova-plugin-console",
-        "clobbers": [
-            "cordova.logger"
-        ]
-    },
-    {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "pluginId": "cordova-plugin-splashscreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
-        "id": "cordova-plugin-statusbar.statusbar",
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "id": "cordova-plugin-x-socialsharing.SocialSharing",
-        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-        "pluginId": "cordova-plugin-x-socialsharing",
-        "clobbers": [
-            "window.plugins.socialsharing"
-        ]
-    }
+  {
+    "id": "cordova-plugin-admobpro.AdMob",
+    "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+    "pluginId": "cordova-plugin-admobpro",
+    "clobbers": [
+      "window.AdMob"
+    ]
+  },
+  {
+    "id": "cordova-plugin-console.console",
+    "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+    "pluginId": "cordova-plugin-console",
+    "clobbers": [
+      "console"
+    ]
+  },
+  {
+    "id": "cordova-plugin-console.logger",
+    "file": "plugins/cordova-plugin-console/www/logger.js",
+    "pluginId": "cordova-plugin-console",
+    "clobbers": [
+      "cordova.logger"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-google-analytics.UniversalAnalytics",
+    "file": "plugins/cordova-plugin-google-analytics/www/analytics.js",
+    "pluginId": "cordova-plugin-google-analytics",
+    "clobbers": [
+      "analytics",
+      "ga"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
+    ]
+  }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-extension": "1.5.1",
-    "cordova-plugin-admobpro": "2.20.1",
-    "cordova-plugin-google-analytics": "1.6.0",
-    "cordova-plugin-inappbrowser": "1.5.0",
-    "cordova-plugin-console": "1.0.4",
-    "cordova-plugin-device": "1.1.3",
-    "cordova-plugin-splashscreen": "4.0.0",
-    "cordova-plugin-statusbar": "2.2.0",
-    "cordova-plugin-x-socialsharing": "5.1.3"
+  "cordova-plugin-extension": "1.5.1",
+  "cordova-plugin-admobpro": "2.20.1",
+  "cordova-plugin-console": "1.0.4",
+  "cordova-plugin-device": "1.1.3",
+  "cordova-plugin-google-analytics": "1.6.0",
+  "cordova-plugin-inappbrowser": "1.5.0",
+  "cordova-plugin-splashscreen": "4.0.0",
+  "cordova-plugin-statusbar": "2.2.0",
+  "cordova-plugin-x-socialsharing": "5.1.3"
 };
 // BOTTOM OF METADATA
 });
