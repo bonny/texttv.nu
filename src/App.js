7fe61c0e57270c389d91a83d2710efcbaa8794fb
Pär Thernström <par.thernstrom@gmail.com>
2019-06-06 15:02:56 +0200

    Visa sidomeny automagiskt på större enheter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-  IonBadge} from "@ionic/react";
+  IonBadge,
+  IonSplitPane
+} from "@ionic/react";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { TabHome } from "./tab-home";
@@ -33,51 +35,53 @@ function App() {
       <Router>
         <div>
           <IonApp>
-            <Menu />
-            <IonPage id="main">
-              <Route exact path="/" render={() => <Redirect to="/hem" />} />
-              <IonTabs>
-                <IonRouterOutlet>
-                  <Route
-                    path="/:tab(hem)"
-                    // component={TabHome}
-                    render={props => (
-                      <TabHome
-                        {...props}
-                        currentTab={currentTab}
-                        prevTab={prevTab}
-                      />
-                    )}
-                    exact={true}
-                  />
-                  <Route
-                    path="/:tab(populart)"
-                    component={TabPopulart}
-                    exact={true}
-                  />
-                  <Route
-                    path="/:tab(nyast)"
-                    component={TabNyast}
-                    exact={true}
-                  />
-                </IonRouterOutlet>
-                <IonTabBar slot="bottom" onClick={handleTabsDidChange}>
-                  <IonTabButton tab="hem" href="/hem">
-                    <IonIcon name="home" />
-                    <IonLabel>Hem c:{currentTab}</IonLabel>
-                    {/* <IonBadge color="danger">6</IonBadge> */}
-                  </IonTabButton>
-                  <IonTabButton tab="populart" href="/populart">
-                    <IonIcon name="trending-up" />
-                    <IonLabel>Populärt p:{prevTab}</IonLabel>
-                  </IonTabButton>
-                  <IonTabButton tab="nyast" href="/nyast">
-                    <IonIcon name="clock" />
-                    <IonLabel>Nyast</IonLabel>
-                  </IonTabButton>
-                </IonTabBar>
-              </IonTabs>
-            </IonPage>
+            <IonSplitPane contentId="main">
+              <Menu />
+              <IonPage id="main">
+                <Route exact path="/" render={() => <Redirect to="/hem" />} />
+                <IonTabs>
+                  <IonRouterOutlet>
+                    <Route
+                      path="/:tab(hem)"
+                      // component={TabHome}
+                      render={props => (
+                        <TabHome
+                          {...props}
+                          currentTab={currentTab}
+                          prevTab={prevTab}
+                        />
+                      )}
+                      exact={true}
+                    />
+                    <Route
+                      path="/:tab(populart)"
+                      component={TabPopulart}
+                      exact={true}
+                    />
+                    <Route
+                      path="/:tab(nyast)"
+                      component={TabNyast}
+                      exact={true}
+                    />
+                  </IonRouterOutlet>
+                  <IonTabBar slot="bottom" onClick={handleTabsDidChange}>
+                    <IonTabButton tab="hem" href="/hem">
+                      <IonIcon name="home" />
+                      <IonLabel>Hem c:{currentTab}</IonLabel>
+                      {/* <IonBadge color="danger">6</IonBadge> */}
+                    </IonTabButton>
+                    <IonTabButton tab="populart" href="/populart">
+                      <IonIcon name="trending-up" />
+                      <IonLabel>Populärt p:{prevTab}</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="nyast" href="/nyast">
+                      <IonIcon name="clock" />
+                      <IonLabel>Nyast</IonLabel>
+                    </IonTabButton>
+                  </IonTabBar>
+                </IonTabs>
+              </IonPage>
+            </IonSplitPane>
           </IonApp>
         </div>
       </Router>
