b0d0faa7e9e56217f80ededae227c7df3469279b
Pär Thernström <par.thernstrom@gmail.com>
2019-06-19 22:13:32 +0200

    Testa med sidor i kolumner

--- src/pages/tab-sidor.jsx
+++ src/pages/tab-sidor.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { IonContent, IonToolbar, IonSearchbar } from "@ionic/react";
+import {
+  IonContent,
+  IonToolbar,
+  IonSearchbar,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonItem,
+  IonLabel
+  
+} from "@ionic/react";
 import TextTVHeader from "../modules/TextTVHeader";
 import TextTVSidorLista from "../modules/TextTVSidorLista";
 
@@ -14,6 +24,42 @@ export default props => {
       document.querySelector("ion-menu-controller").close();
     }
   };
+
+  const OneItem = (props) => {
+    const {lines = 'inset', title, pages} = props;
+    const url = `/sida/${pages}`;
+    return (
+      <IonItem
+      button
+      detail
+      onClick={() => {
+        document.querySelector("ion-menu-controller").close();
+        history.push(url);
+      }}
+      lines={lines}
+      color="dark"
+    >
+      <IonLabel text-wrap>
+        <h2 className="ListHeadlineSidor">{title}</h2>
+        <p>{pages}</p>
+      </IonLabel>
+    </IonItem>
+    )};
+
+    const OneHeadlineItem = (props) => {
+      const itemStyles = {
+        color: 'var(--text-tv-color-cyan)',
+      }
+      const {title} = props;
+      return (
+        <IonCol size="12">
+        <IonItem lines="none" style={itemStyles}>
+<IonLabel>{title}</IonLabel>
+</IonItem>
+        </IonCol>
+
+      );
+    }
 
   return (
     <>
@@ -31,6 +77,44 @@ export default props => {
         </IonToolbar>
       </TextTVHeader>
       <IonContent color="dark">
+
+        <IonGrid no-padding>
+
+        <IonRow>
+        <OneHeadlineItem title="Start"/>
+            <IonCol size="6"><OneItem title="Nyheter" pages="100" /></IonCol>
+            <IonCol size="6"><OneItem title="Översikt" pages="700" /></IonCol>
+          </IonRow>
+
+          <IonRow>
+            <OneHeadlineItem title="Nyheter"/>
+            <IonCol size="6"><OneItem title="Inrikes" pages="101-103" /></IonCol>
+            <IonCol size="6"><OneItem title="Utrikes" pages="104-105" /></IonCol>
+          </IonRow>
+
+          <IonRow>
+
+          <OneHeadlineItem title="Sport"/>
+                      <IonCol size="6"><OneItem title="Nyheter" pages="300-302" /></IonCol>
+            <IonCol size="6"><OneItem title="Resultatbörs" pages="330" /></IonCol>
+            <IonCol size="6"><OneItem title="Målservice" pages="376-395" /></IonCol>
+            <IonCol size="6"><OneItem title="Sport i SVT" pages="379" /></IonCol>
+          </IonRow>
+          <IonRow>
+          <OneHeadlineItem title="Ekonomi"/>
+
+            <IonCol size="6"><OneItem title="Innehåll" pages="200-201" /></IonCol>
+            <IonCol size="6"><OneItem title="Börsen" pages="202" /></IonCol>
+          </IonRow>
+          <IonRow>
+          <OneHeadlineItem title="TV"/>
+
+            <IonCol size="6"><OneItem title="Tablåer" pages="600" /></IonCol>
+            <IonCol size="6"><OneItem title="Programguiden" pages="650" /></IonCol>
+            <IonCol size="6"><OneItem title="Dagens programsidor" pages="624" /></IonCol>
+          </IonRow>
+        </IonGrid>
+
         <TextTVSidorLista {...props} showHeader={false} />
       </IonContent>
     </>
