856108a120e311600b53091f342844a78fd58038
Pär Thernström <par.thernstrom@gmail.com>
2019-06-22 23:52:58 +0200

    Sökrutefixar

--- src/modules/TextTVSearchBar.js
+++ src/modules/TextTVSearchBar.js
@@ -1,27 +1,35 @@
 import { IonSearchbar } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 
 const TextTVSearchBar = props => {
   const { history } = props;
+  const [isLoading, setIsLoading] = useState(false);
 
   const handlePageNumInputChange = e => {
     const pageNum = e.target.value;
     if (pageNum.length === 3) {
+      setIsLoading(true);
       history.push(`/sida/${pageNum}`);
-      e.target.value = "";
       document.querySelector("ion-menu-controller").close();
+      // e.target.value = "";
     }
   };
 
+  // const icon = isLoading ? "document" : "search";
+
   return (
-    <IonSearchbar
-      color="secondary"
-      placeholder="Gå till…"
-      type="number"
-      showCancelButton={false}
-      clearIcon={false}
-      onIonChange={handlePageNumInputChange}
-    />
+    <>
+      <IonSearchbar
+        color="secondary"
+        placeholder="Gå till…"
+        type="number"
+        showCancelButton={false}
+         clearIcon={false}
+        onIonChange={handlePageNumInputChange}
+        debounce="100"
+        disabled={isLoading}
+      />
+    </>
   );
 };
 
