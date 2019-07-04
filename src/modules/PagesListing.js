a673cb936aa2e1600b436da41b63eb9b524f43d1
Pär Thernström <par.thernstrom@gmail.com>
2019-07-04 21:46:48 +0200

    Gör list item till egen komponent

--- src/modules/PagesListing.js
+++ src/modules/PagesListing.js
@@ -1,7 +1,33 @@
 import { IonItem, IonLabel } from "@ionic/react";
 import React from "react";
+// import { withRouter } from "react-router";
 import "moment/locale/sv";
 import Moment from "react-moment";
+
+const ListItem = props => {
+  const { link, page, lines, history } = props;
+  return (
+    <IonItem
+      button
+      detail
+      onClick={e => {
+        console.log("pushed link", link, history);
+        history.push(link);
+      }}
+      lines={lines}
+      color="dark"
+    >
+      <IonLabel text-wrap>
+        <h2 className="ListHeadline">{page.title}</h2>
+        <p>
+          <Moment unix fromNow locale="sv" className="MomentTime">
+            {page.date_added_unix}
+          </Moment>
+        </p>
+      </IonLabel>
+    </IonItem>
+  );
+};
 
 const PagesListing = props => {
   // linkto = pagenum | pageid
@@ -22,25 +48,14 @@ const PagesListing = props => {
     }
 
     return (
-      <IonItem
-        button
-        detail
-        onClick={e => {
-          history.push(link);
-        }}
-        key={page.id}
+      <ListItem
+        {...props}
+        link={link}
+        page={page}
         lines={lines}
-        color="dark"
-      >
-        <IonLabel text-wrap>
-          <h2 className="ListHeadline">{page.title}</h2>
-          <p>
-            <Moment unix fromNow locale="sv" className="MomentTime">
-              {page.date_added_unix}
-            </Moment>
-          </p>
-        </IonLabel>
-      </IonItem>
+        key={page.id}
+        passedHistory={history}
+      />
     );
   });
 };
