4a92bf1fb13c2e5e6b5d54f0891b7c449657fd74
Pär Thernström <par.thernstrom@gmail.com>
2019-08-17 12:49:06 +0200

    Flytta navItems och navItemsAlsoLike till egen fil

---
+++ src/modules/navItems.js
@@ -0,0 +1,65 @@
+import { alert, document, help, informationCircleOutline, logoFacebook, logoTwitter, paper } from "ionicons/icons";
+export const navItems = [
+  {
+    title: "Best of SVT Text",
+    href: "http://bestofsvttext.eskapism.se",
+    icon: document
+  },
+  {
+    title: "TextTV.nu på Facebook",
+    href: "https://www.facebook.com/texttv.nu/",
+    icon: logoFacebook
+  },
+  {
+    title: "Utvecklingsblogg",
+    href: "https://texttv.nu/blogg",
+    icon: informationCircleOutline
+  },
+  {
+    title: "TextTV.nu på Twitter",
+    href: "https://twitter.com/texttv_nu",
+    icon: logoTwitter
+  },
+  {
+    title: "@text_tv_sport",
+    href: "https://twitter.com/text_tv_sport",
+    icon: logoTwitter
+  },
+  {
+    title: "@text_tv_nyheter",
+    href: "https://twitter.com/text_tv_nyheter",
+    icon: logoTwitter
+  },
+  {
+    title: "Omnämnt av Polisen",
+    href: "https://texttv.nu/sida/polisen",
+    icon: alert
+  },
+  {
+    title: "Om TextTV.nu",
+    href: "https://texttv.nu/sida/om-texttv-nu",
+    icon: help
+  }
+];
+export const navItemsAlsoLike = [
+  {
+    title: "SVT Nyheter",
+    href: "https://www.svt.se/",
+    icon: paper
+  },
+  {
+    title: "SVT Nyheter på Twitter",
+    href: "https://twitter.com/svtnyheter",
+    icon: logoTwitter
+  },
+  {
+    title: "SVT Sport på Twitter",
+    href: "https://twitter.com/SVTSport",
+    icon: logoTwitter
+  },
+  {
+    title: "❤️ 377",
+    href: "https://twitter.com/svt377",
+    icon: logoTwitter
+  }
+];
