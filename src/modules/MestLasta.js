162b8ccb8c51b798d1f956669895d5f2a0dfd274
Pär Thernström <par.thernstrom@gmail.com>
2019-10-12 16:52:41 +0200

    Fixa no-lone-blocks

--- src/modules/MestLasta.js
+++ src/modules/MestLasta.js
@@ -5,92 +5,90 @@ import PagesListing from "./PagesListing";
 import SkeletonList from "./SkeletonList";
 
 const MestLastaLista = props => {
-  {
-    const { history, day = "today", refreshTime, count = 15 } = props;
-    const [isLoading, setIsLoading] = useState(true);
-    const [isLoadingError, setIsLoadingError] = useState(false);
-    const [pages, setPages] = useState([]);
+  const { history, day = "today", refreshTime, count = 15 } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingError, setIsLoadingError] = useState(false);
+  const [pages, setPages] = useState([]);
 
-    // Fetch content for segment when segment day is changed.
-    useEffect(() => {
-      let isUnmounted = false;
-      let dateYMD;
-      let todayYMD = dayjs().format("YYYY-MM-DD");
-      let yesterdayYMD = dayjs()
-        .subtract(1, "day")
-        .format("YYYY-MM-DD");
-      let dayBeforeYesterdayYMD = dayjs()
-        .subtract(2, "day")
-        .format("YYYY-MM-DD");
+  // Fetch content for segment when segment day is changed.
+  useEffect(() => {
+    let isUnmounted = false;
+    let dateYMD;
+    let todayYMD = dayjs().format("YYYY-MM-DD");
+    let yesterdayYMD = dayjs()
+      .subtract(1, "day")
+      .format("YYYY-MM-DD");
+    let dayBeforeYesterdayYMD = dayjs()
+      .subtract(2, "day")
+      .format("YYYY-MM-DD");
 
-      switch (day) {
-        case "dayBeforeYesterday":
-          dateYMD = dayBeforeYesterdayYMD;
-          break;
-        case "yesterday":
-          dateYMD = yesterdayYMD;
-          break;
-        case "today":
-        default:
-          dateYMD = todayYMD;
-          break;
-      }
-
-      const endpoint = `https://texttv.nu/api/most_read/news?count=${count}&date=${dateYMD}`;
-
-      setIsLoading(true);
-      setIsLoadingError(false);
-      setPages([]);
-
-      fetch(endpoint)
-        .then(data => {
-          return data.json();
-        })
-        .then(data => {
-          // Bail if component already unmounted.
-          if (isUnmounted) {
-            setIsLoading(false);
-            return;
-          }
-
-          setIsLoading(false);
-          setPages(data.pages);
-        })
-        .catch(error => {
-          // Network error or similar.
-          setIsLoadingError(true);
-        });
-
-      return e => {
-        isUnmounted = true;
-      };
-    }, [day, count, refreshTime]);
-
-    const Pages = (
-      <PagesListing pages={pages} history={history} linkTo="pageid" />
-    );
-
-    let Knastext = null;
-
-    if (isLoadingError || (!isLoading && pages.length === 0)) {
-      Knastext = (
-        <div className="ion-padding">
-          <p>
-            Hm, det gick inte att hämta de mest lästa sidorna. Försök igen om en
-            liten stund.
-          </p>
-        </div>
-      );
+    switch (day) {
+      case "dayBeforeYesterday":
+        dateYMD = dayBeforeYesterdayYMD;
+        break;
+      case "yesterday":
+        dateYMD = yesterdayYMD;
+        break;
+      case "today":
+      default:
+        dateYMD = todayYMD;
+        break;
     }
 
-    return (
-      <>
-        {isLoading && SkeletonList}
-        {Knastext}
-        {Pages && <IonList>{Pages}</IonList>}
-      </>
+    const endpoint = `https://texttv.nu/api/most_read/news?count=${count}&date=${dateYMD}`;
+
+    setIsLoading(true);
+    setIsLoadingError(false);
+    setPages([]);
+
+    fetch(endpoint)
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        // Bail if component already unmounted.
+        if (isUnmounted) {
+          setIsLoading(false);
+          return;
+        }
+
+        setIsLoading(false);
+        setPages(data.pages);
+      })
+      .catch(error => {
+        // Network error or similar.
+        setIsLoadingError(true);
+      });
+
+    return e => {
+      isUnmounted = true;
+    };
+  }, [day, count, refreshTime]);
+
+  const Pages = (
+    <PagesListing pages={pages} history={history} linkTo="pageid" />
+  );
+
+  let Knastext = null;
+
+  if (isLoadingError || (!isLoading && pages.length === 0)) {
+    Knastext = (
+      <div className="ion-padding">
+        <p>
+          Hm, det gick inte att hämta de mest lästa sidorna. Försök igen om en
+          liten stund.
+        </p>
+      </div>
     );
   }
+
+  return (
+    <>
+      {isLoading && SkeletonList}
+      {Knastext}
+      {Pages && <IonList>{Pages}</IonList>}
+    </>
+  );
 };
 
 export default props => {
--- src/modules/SenastUppdaterat.js
+++ src/modules/SenastUppdaterat.js
@@ -4,76 +4,74 @@ import SkeletonList from "./SkeletonList";
 import PagesListing from "./PagesListing";
 
 const SenastUppdateradeLista = props => {
-  {
-    const { type = "news", history, refreshTime, count = 15 } = props;
-    const [isLoading, setIsLoading] = useState(false);
-    const [isLoadingError, setIsLoadingError] = useState(false);
-    const [pages, setPages] = useState([]);
+  const { type = "news", history, refreshTime, count = 15 } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingError, setIsLoadingError] = useState(false);
+  const [pages, setPages] = useState([]);
 
-    // Fetch content for segment when segment type is changed.
-    useEffect(() => {
-      const endpoints = [
-        {
-          what: "news",
-          endpoint: `https://texttv.nu/api/last_updated/news?count=${count}`
-        },
-        {
-          what: "sports",
-          endpoint: `https://texttv.nu/api/last_updated/sport?count=${count}`
+  // Fetch content for segment when segment type is changed.
+  useEffect(() => {
+    const endpoints = [
+      {
+        what: "news",
+        endpoint: `https://texttv.nu/api/last_updated/news?count=${count}`
+      },
+      {
+        what: "sports",
+        endpoint: `https://texttv.nu/api/last_updated/sport?count=${count}`
+      }
+    ];
+
+    let isUnmounted = false;
+
+    let endpoint = endpoints.find(source => {
+      return source.what === type;
+    });
+
+    setIsLoading(true);
+    setIsLoadingError(false);
+    setPages([]);
+
+    fetch(endpoint.endpoint)
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        setIsLoading(false);
+
+        // Bail if component already unmounted.
+        if (isUnmounted) {
+          return;
         }
-      ];
 
-      let isUnmounted = false;
-
-      let endpoint = endpoints.find(source => {
-        return source.what === type;
+        setPages(data.pages);
+      })
+      .catch(error => {
+        // Network error or similar.
+        setIsLoadingError(true);
       });
 
-      setIsLoading(true);
-      setIsLoadingError(false);
-      setPages([]);
+    return e => {
+      isUnmounted = true;
+    };
+  }, [type, count, refreshTime]);
 
-      fetch(endpoint.endpoint)
-        .then(data => {
-          return data.json();
-        })
-        .then(data => {
-          setIsLoading(false);
+  const Pages = <PagesListing pages={pages} history={history} />;
 
-          // Bail if component already unmounted.
-          if (isUnmounted) {
-            return;
-          }
-
-          setPages(data.pages);
-        })
-        .catch(error => {
-          // Network error or similar.
-          setIsLoadingError(true);
-        });
-
-      return e => {
-        isUnmounted = true;
-      };
-    }, [type, count, refreshTime]);
-
-    const Pages = <PagesListing pages={pages} history={history} />;
-
-    return (
-      <>
-        {isLoading && SkeletonList}
-        {isLoadingError && <p>Det blev ett fel vid laddning ...</p>}
-        {Pages && (
-          <IonList>
-            {/* <IonListHeader>
+  return (
+    <>
+      {isLoading && SkeletonList}
+      {isLoadingError && <p>Det blev ett fel vid laddning ...</p>}
+      {Pages && (
+        <IonList>
+          {/* <IonListHeader>
               <IonLabel>Nyligen uppdaterat</IonLabel>
             </IonListHeader> */}
-            {Pages}
-          </IonList>
-        )}
-      </>
-    );
-  }
+          {Pages}
+        </IonList>
+      )}
+    </>
+  );
 };
 
 export default props => {
