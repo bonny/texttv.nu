99a1b0db3799d7376bd4d506447fdf536b0f46bc
Pär Thernström <par.thernstrom@gmail.com>
2020-02-03 22:36:40 +0100

    Använd nya och förhoppningsvis korrekta admob-värden

--- src/options.js
+++ src/options.js
@@ -1,10 +1,11 @@
 const adMobAdOptions = {
-  adId: "ca-app-pub-1689239266452655/3336016805",
+  adId: "ca-app-pub-1689239266452655/6481628543",
   // google test ad
   // https://developers.google.com/admob/android/test-ads#sample_ad_units
   // adId: "ca-app-pub-3940256099942544/6300978111",
   adSize: "SMART_BANNER",
-  position: "BOTTOM_CENTER"
+  position: "BOTTOM_CENTER",
+  isTesting: false
 };
 
 export { adMobAdOptions };
