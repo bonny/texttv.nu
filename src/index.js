78d4140d70b3c5f29ad267644bf37255b539d9c1
Pär Thernström <par.thernstrom@gmail.com>
2021-05-22 10:13:14 +0200

    Inaktivera service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import TextTVApp from "./TextTVApp";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <TextTVApp />,
@@ -10,4 +10,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
