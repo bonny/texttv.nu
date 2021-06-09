f979182200cdaf1e3a01026b438040304a8b0c07
Pär Thernström <par.thernstrom@gmail.com>
2021-06-09 20:33:21 +0200

    Ångra aktivering av service worker pga har inte rätt fil

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom";
 import TextTVApp from "./TextTVApp";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<TextTVApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
