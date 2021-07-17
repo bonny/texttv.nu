068a365da0773b73c634166b08ce369c80527b00
Pär Thernström <par.thernstrom@gmail.com>
2021-07-17 14:29:22 +0200

    Förenkla kod

--- src/modules/TextTVSearchBar.js
+++ src/modules/TextTVSearchBar.js
@@ -1,6 +1,9 @@
 import { IonSearchbar } from "@ionic/react";
 import { FirebaseAnalytics } from "../analytics";
 
+/**
+ * "Sökruta"/gå-till-sida-input som ska ligga i IonHeader > IonToolbar.
+ */
 const TextTVSearchBar = (props) => {
   const { history } = props;
 
@@ -8,23 +11,27 @@ const TextTVSearchBar = (props) => {
     const target = e.target;
     const pageNum = target.value;
 
-    if (pageNum.length === 3) {
-      target.value = "";
-      history.push(`/sidor/${pageNum}`);
-
-      e.target.getInputElement().then((elm) => {
-        elm.blur();
-      });
-
-      try {
-        FirebaseAnalytics.logEvent({
-          name: "go_to_page_manually",
-          params: {
-            page_num: pageNum,
-          },
-        });
-      } catch (e) {}
+    // Baila om vi inte har tre tecken, dvs. ett sidnummer.
+    if (pageNum.length !== 3) {
+      return;
     }
+
+    target.value = "";
+    history.push(`/sidor/${pageNum}`);
+
+    e.target.getInputElement().then((elm) => {
+      elm.blur();
+    });
+
+    try {
+      FirebaseAnalytics.logEvent({
+        name: "go_to_page",
+        params: {
+          type: "manually",
+          page_num: pageNum,
+        },
+      });
+    } catch (e) {}
   };
 
   return (
