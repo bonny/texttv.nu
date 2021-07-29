6a278e8eee3c149c25272a1eabe27693f9fff23c
Pär Thernström <par.thernstrom@gmail.com>
2021-07-29 20:24:04 +0200

    Ta bort debug för admob id

--- src/adMobAdOptions.js
+++ src/adMobAdOptions.js
@@ -3,7 +3,6 @@ import { isPlatform } from "@ionic/react";
 const adIdIos = "ca-app-pub-1689239266452655/6481628543";
 const adIdAndroid = "ca-app-pub-1689239266452655/7602900801";
 const adId = isPlatform("android") ? adIdAndroid : adIdIos;
-console.log("admob adid is", adId);
 
 const adMobAdOptions = {
   // Banner: TextTV.nu IOS React App
