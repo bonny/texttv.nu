0709f88b22ef7e0f95bf9b2a25c24da0911e9130
Pär Thernström <par.thernstrom@gmail.com>
2021-08-22 18:58:31 +0200

    Flytta brödsmulor till egen fil

--- src/modules/TextTVPage.jsx
+++ src/modules/TextTVPage.jsx
@@ -2,97 +2,17 @@
  * En text-tv-sida som visas i en IonPage.
  * Visas i en befintlig sida alltså.
  */
+import { isPlatform } from "@ionic/react";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
-import { isPlatform, IonIcon } from "@ionic/react";
-import { document, home, homeOutline, homeSharp } from "ionicons/icons";
-import { Link } from "react-router-dom";
 import {
   createMarkupForPage,
   getCacheBustTimeString,
   getNearestLink,
-  hidePageUpdatedToasts,
-  sendStats,
-  logPageView,
+  hidePageUpdatedToasts, logPageView, sendStats
 } from "../functions";
 import SkeletonTextTVPage from "../modules/SkeletonTextTVPage";
-
-const TextTVPageBreadcrumbs = (props) => {
-  const firstPageBreadcrumbs = props?.pageData?.slice(0, 1)?.pop()?.breadcrumbs;
-
-  if (!firstPageBreadcrumbs || !firstPageBreadcrumbs.length) {
-    return null;
-  }
-
-  const listStyles = {
-    // borderTop: "1px solid #222",
-    listStyle: "none",
-    padding: "0 0 .5em 0",
-    margin: "0 0 .5em .4em",
-    textAlign: "left",
-    borderBottom: "1px solid #222",
-    fontSize: "1.15rem",
-    verticalAlign: "middle",
-  };
-
-  const listItemStyles = {
-    // outline: "1px solid red",
-    display: "inline-block",
-    // verticalAlign: "middle",
-    // lineHeight: 1
-  };
-
-  const linkStyles = {
-    // outline: "1px solid pink",
-    display: "inline-block",
-    padding: ".5em .5em .75em .5em",
-    color: "#777",
-    xtextDecoration: "none",
-    textUnderlineOffset: "4px",
-  };
-
-  const itemDivider = {
-    // outline: "1px solid green",
-    padding: "0 .5em",
-    margin: 0,
-    color: "#444",
-    // verticalAlign: "middle",
-  };
-
-  const lastItemStyles = {
-    ...linkStyles,
-  };
-
-  const iconStyles = {
-    height: "1.8ex",
-    position: "relative",
-    top: "7px",
-    padding: "0 0 3px 0",
-    borderBottom: "2px solid #777",
-  };
-
-  const breadcrumbs = firstPageBreadcrumbs.map((crumb, idx, arr) => {
-    return (
-      <li key={crumb.num} style={listItemStyles}>
-        {idx < arr.length - 1 && (
-          <>
-            <Link to={crumb.url} style={linkStyles}>
-              {idx === 0 && <IonIcon style={iconStyles} icon={homeSharp} />}
-              {idx > 0 && crumb.name}
-            </Link>
-
-            <span style={itemDivider}>/</span>
-          </>
-        )}
-        {idx === arr.length - 1 && (
-          <span style={lastItemStyles}>{crumb.name}</span>
-        )}
-      </li>
-    );
-  });
-
-  return <ol style={listStyles}>{breadcrumbs}</ol>;
-};
+import { TextTVPageBreadcrumbs } from "./TextTVPageBreadcrumbs";
 
 const TextTVPage = (props) => {
   const { pageNum, pageId, children, history, refreshTime, onPageUpdate } =
