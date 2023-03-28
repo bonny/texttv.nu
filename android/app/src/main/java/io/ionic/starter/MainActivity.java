64d5748cfbdb9992e545da467f509fc5deaf69ab
Pär Thernström <par.thernstrom@gmail.com>
2023-03-28 21:07:21 +0200

    Uppdatera till Capacitor 4

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -18,16 +18,7 @@ public class MainActivity extends BridgeActivity {
   // public void onCreate(Bundle savedInstanceState) {
   //   super.onCreate(savedInstanceState);
 
-  //   // Initializes the Bridge
-  //   this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-  //     // Additional plugins you've installed go here
-  //     // Ex: add(TotallyAwesomePlugin.class);
-
-  //     // Add AdMob as a Capacitor Plugin.
-  //     registerPlugin(com.getcapacitor.community.admob.AdMob.class);
-
-  //     // Add Capacitor Firebase analytics plugin
-  //     add(FirebaseAnalytics.class);
-  //   }});
+  //   
+  //   
   // }
 }
