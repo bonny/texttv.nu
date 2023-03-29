8dd2bc59dc6142e50364e822056b4e38fbf2c369
Pär Thernström <par.thernstrom@gmail.com>
2023-03-29 20:29:52 +0200

    Mounta react-root på version 18-sätt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import TextTVApp from "./TextTVApp";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<TextTVApp />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<TextTVApp />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
